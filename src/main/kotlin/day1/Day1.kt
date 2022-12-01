421b0dcd1b72682a78b11879344982b296b207d4
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2022-12-01 20:44:35 +0100

    Solve part 2 day1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -2,9 +2,16 @@ package day1
 
 import java.io.File
 
-fun maxCalories(filename: String) : Int {
+fun maxCaloriesFirstElf(filename: String) : Int {
     val input = File("src/main/resources/$filename").readText()
     return input.split("\n\n").maxOf { it.toNumbers().sum() }
 }
 
 fun String.toNumbers() : List<Int> = this.split("\n").map(String::toInt)
+
+
+fun maxCaloriesFirstThreeElves(filename: String) : Int {
+    val input = File("src/main/resources/$filename").readText()
+    val sums = input.split("\n\n").map { it.toNumbers().sum() }
+    return sums.sorted().takeLast(3).sum()
+}
