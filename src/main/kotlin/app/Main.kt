785673b4ee487b810ceadf224d47b4c109f73d13
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2022-12-01 21:19:29 +0100

    create day1 function to print results of day1 in main

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -5,9 +5,19 @@ import java.io.File
 private const val resources = "src/main/resources/"
 
 fun main() {
-    val input = readInput("input.txt")
-    println(maxCaloriesFirstElf(input))
-    println(maxCaloriesFirstThreeElves(input))
+    day1("inputDay1.txt")
+}
+
+private fun day1(filename: String) {
+    executeWithInput(filename) {
+        println(maxCaloriesFirstElf(it))
+        println(maxCaloriesFirstThreeElves(it))
+    }
+}
+
+private fun executeWithInput(filename: String, action: (String) -> Unit) {
+    val input = readInput(filename)
+    action(input)
 }
 
 private fun readInput(filename: String): String {
--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,15 +1,13 @@
 package day1
 
-import java.io.File
-
 fun maxCaloriesFirstElf(input: String) : Int {
     return input.split("\n\n").maxOf { it.toNumbers().sum() }
 }
-
-fun String.toNumbers() : List<Int> = this.split("\n").map(String::toInt)
-
 
 fun maxCaloriesFirstThreeElves(input: String) : Int {
     val sums = input.split("\n\n").map { it.toNumbers().sum() }
     return sums.sorted().takeLast(3).sum()
 }
+
+
+fun String.toNumbers() : List<Int> = this.split("\n").map(String::toInt)
