351b3e5f4d8f0701d9901a14dd09bdd650518767
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2022-12-03 15:27:31 +0100

    initialize day3

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -1,13 +1,13 @@
-import day1.maxCaloriesFirstElf
-import day1.maxCaloriesFirstThreeElves
-import day2.Day2
+
+import day3.Day3
 import java.io.File
 
 private const val resources = "src/main/resources/"
 
 fun main() {
     //executeWithInput("inputDay1.txt", ::maxCaloriesFirstElf, ::maxCaloriesFirstThreeElves)
-    executeWithInput("inputDay2.txt", Day2::part1, Day2::part2)
+//    executeWithInput("inputDay2.txt", Day2::part1, Day2::part2)
+    executeWithInput("inputDay3.txt", Day3::part1, Day3::part2)
 }
 
 private fun executeWithInput(filename: String, part1Action: (String) -> Any, part2Action: (String) -> Any) {
---
+++ src/main/kotlin/day3/Day3.kt
@@ -0,0 +1,6 @@
+package day3
+
+object Day3 {
+    fun part1(input : String) {}
+    fun part2(input : String) {}
+}
