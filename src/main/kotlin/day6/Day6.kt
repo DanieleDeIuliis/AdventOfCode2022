a25b4ee69bb5dfba0fe17b562c9cc8d56b8374a7
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2022-12-06 18:15:58 +0100

    day6 part1

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -2,7 +2,13 @@ package day6
 
 object Day6 {
     fun part1(input: String): Int {
-        return 1
+        repeat(input.length - 3) {
+            val distinctChars = input.subSequence(it, it + 4).toSet()
+            if(distinctChars.size == 4) {
+                return it + 4
+            }
+        }
+        return -1
     }
 
     fun part2(input: String): Int {
--- src/test/kotlin/day6/Day6Test.kt
+++ src/test/kotlin/day6/Day6Test.kt
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test
 class Day6Test {
     @Test
     fun `part 1`() {
-
+        assertThat(Day6.part1("npmn")).isEqualTo(-1)
+        assertThat(Day6.part1("nnmpa")).isEqualTo(5)
         assertThat(Day6.part1("nppdvjthqldpwncqszvftbrmjlhg")).isEqualTo(6)
         assertThat(Day6.part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).isEqualTo(10)
         assertThat(Day6.part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).isEqualTo(11)
