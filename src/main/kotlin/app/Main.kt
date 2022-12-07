72a14c6ae2dab5289fca0579c5326bd093e686ce
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2022-12-07 19:26:20 +0100

    init day7

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -1,5 +1,5 @@
 
-import day6.Day6
+import day7.Day7
 import java.io.File
 
 private const val resources = "src/main/resources/"
@@ -10,7 +10,8 @@ fun main() {
     //executeWithInput("inputDay3.txt", Day3::part1, Day3::part2)
     //executeWithInput("inputDay4.txt", Day4::part1, Day4::part2)
     //executeWithInput("inputDay5.txt", Day5::part1, Day5::part2)
-    executeWithInput("inputDay6.txt", Day6::part1, Day6::part2)
+    //executeWithInput("inputDay6.txt", Day6::part1, Day6::part2)
+    executeWithInput("inputDay7.txt", Day7::part1, Day7::part2)
 
 }
 
---
+++ src/main/kotlin/day7/Day7.kt
@@ -0,0 +1,11 @@
+package day7
+
+object Day7 {
+    fun part1(input: String): Int {
+        return 13515643
+    }
+
+    fun part2(input: String): Int {
+        return 912481248
+    }
+}
