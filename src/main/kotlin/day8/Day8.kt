34cf7292162b2ba3fe134ae3c1d0df2d3692c41e
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2022-12-08 19:28:52 +0100

    awful day8 part1

--- src/main/kotlin/day8/Day8.kt
+++ src/main/kotlin/day8/Day8.kt
@@ -2,7 +2,54 @@ package day8
 
 object Day8 {
     fun part1(input: String): Int {
-        return 4251321
+        val rows = input.split("\n").map {
+            it.toCharArray().map(Char::digitToInt)
+        }
+
+        val cols = List(rows[0].size) {col ->
+            List(rows[0].size) { row ->
+                rows[row][col]
+            }
+        }
+
+        var count = 0
+        rows.forEachIndexed { rowIndex, rows ->
+            rows.forEachIndexed { colIndex, _ ->
+                val left = isVisibleOnLeft(rows, colIndex)
+                val right = isVisibleOnRight(rows, colIndex)
+                val up = isVisibleOnTop(cols[colIndex], rowIndex)
+                val down = isVisibleOnBottom(cols[colIndex], rowIndex)
+                if(left || right || up || down) count++
+            }
+        }
+
+        return count
+    }
+
+    fun isVisibleOnLeft(row: List<Int>, index: Int): Boolean {
+        if(index == 0) return true
+        val value = row[index]
+        return row.take(index).all { it < value }
+    }
+
+    fun isVisibleOnRight(row: List<Int>, index: Int): Boolean {
+        if(index == row.size - 1) return true
+        val value = row[index]
+        return row.drop(index + 1).all { it < value }
+
+    }
+
+    fun isVisibleOnTop(col: List<Int>, index: Int): Boolean {
+        if(index == 0) return true
+        val value = col[index]
+        return col.take(index).all { it < value }
+    }
+
+    fun isVisibleOnBottom(col: List<Int>, index: Int): Boolean {
+        if(index == col.size - 1) return true
+        val value = col[index]
+        return col.drop(index + 1).all { it < value }
+
     }
 
     fun part2(input: String): Int {
