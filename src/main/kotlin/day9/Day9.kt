b6c41f8aa69302e335aff6001cd8a70e8e126b8c
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2022-12-09 21:55:51 +0100

    day9 part 2

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -19,12 +19,11 @@ object Day9 {
             repeat(movement.times) {
                 head.move(movement.direction)
                 if(tooDistant(head, tail)) {
-                    tail.getCloser(head, movement.direction)
+                    tail.getCloser(head)
                     visited.add(tail.copy())
                 }
             }
         }
-
         return visited.size
     }
 
@@ -34,7 +33,31 @@ object Day9 {
     }
 
     fun part2(input: String): Int {
-        return 123
+        val movements = input.split("\n").map { movement ->
+            val (direction, times) = movement.trim().split(" ")
+            Movement(direction, times.toInt())
+        }
+
+        val visited = mutableSetOf(Position(0, 0))
+
+        val nodes = List(10) { Position(0,0) }
+
+        movements.forEach { movement ->
+            repeat(movement.times) {
+                nodes[0].move(movement.direction)
+                for(index in 0 until nodes.size - 1){
+                    val head = nodes[index]
+                    val tail = nodes[index + 1]
+                    if (tooDistant(head, tail)) {
+                        tail.getCloser(head)
+                        if(index + 1 == nodes.size - 1) {
+                            visited.add(tail.copy())
+                        }
+                    }
+                }
+            }
+        }
+        return visited.size
     }
 
     data class Movement(val direction: String, val times: Int)
@@ -50,32 +73,47 @@ object Day9 {
         }
     }
 
-    private fun Position.getCloser(head: Position, direction: String) {
-        if(row == head.row || col == head.col) {
-            move(direction)
-        } else {
-            when(direction) {
-                "R" -> {
-                    col += 1
-                    followRow(head)
-                }
-                "L" -> {
-                    col -= 1
-                    followRow(head)
-                }
-                "U" -> {
-                    row += 1
-                    followCol(head)
-                }
-                "D" -> {
-                    row -= 1
-                    followCol(head)
-                }
+    private fun Position.getCloser(to: Position) {
+        when {
+            row == to.row -> getCloserHorizontally(to)
+            col == to.col -> getCloserVertically(to)
+            else -> getCloserDiagonally(to)
+        }
+    }
+
+    private fun Position.getCloserDiagonally(to: Position) {
+        when {
+            needsToNorthEast(to) -> {
+                col += 1
+                row += 1
+            }
+
+            needsToGoNorthWest(to) -> {
+                col -= 1
+                row += 1
+            }
+
+            needsToGoSouthWest(to) -> {
+                row -= 1
+                col -= 1
+            }
+
+            needsToGoSouthEast(to) -> {
+                row -= 1
+                col += 1
             }
         }
     }
 
-    private fun Position.followCol(head: Position) {
+    private fun Position.needsToGoSouthEast(head: Position) = row > head.row && col < head.col
+
+    private fun Position.needsToGoSouthWest(head: Position) = row > head.row && col > head.col
+
+    private fun Position.needsToGoNorthWest(head: Position) = row < head.row && col > head.col
+
+    private fun Position.needsToNorthEast(head: Position) = row < head.row && col < head.col
+
+    private fun Position.getCloserHorizontally(head: Position) {
         if (col > head.col ) {
             col -= 1
         } else {
@@ -83,7 +121,7 @@ object Day9 {
         }
     }
 
-    private fun Position.followRow(head: Position) {
+    private fun Position.getCloserVertically(head: Position) {
         if (row > head.row) {
             row -= 1
         } else {
--- src/test/kotlin/day9/Day9Test.kt
+++ src/test/kotlin/day9/Day9Test.kt
@@ -22,4 +22,40 @@ class Day9Test {
 
         assertThat(result).isEqualTo(13)
     }
+
+    @Test
+    fun `part 2`() {
+        val input = """
+            R 5
+            U 8
+            L 8
+            D 3
+            R 17
+            D 10
+            L 25
+            U 20
+        """.trimIndent()
+
+        val result = Day9.part2(input)
+
+        assertThat(result).isEqualTo(36)
+    }
+
+    @Test
+    fun `part 2 piu piccolo`() {
+        val input = """
+            R 4
+            U 4
+            L 3
+            D 1
+            R 4
+            D 1
+            L 5
+            R 2
+        """.trimIndent()
+
+        val result = Day9.part2(input)
+
+        assertThat(result).isEqualTo(1)
+    }
 }
