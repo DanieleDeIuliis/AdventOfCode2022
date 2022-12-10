5656d327337912315c163fed14baa946626ec5cf
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2022-12-10 21:32:38 +0100

    day10 part2

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -1,18 +1,14 @@
 package day10
 
+import kotlin.math.absoluteValue
+
 object Day10 {
     fun part1(input: String): Int {
         var numberOfCycles = 0
         var valueOfRegister = 1
         var currentCycleToMultiply = 20
         var result = 0
-        val operations = input.split("\n").flatMap {
-            if(it.trim() == "noop") {
-                listOf(Operation())
-            } else {
-                listOf(Operation(), Operation(it.trim().split(" ").last().toInt()))
-            }
-        }
+        val operations = extractOperations(input)
 
         operations.forEach {
             numberOfCycles += 1
@@ -28,9 +24,38 @@ object Day10 {
         return result
     }
 
-    fun part2(input: String): Int {
-        return 124
+    private fun extractOperations(input: String): List<Operation> {
+        val operations = input.split("\n").flatMap {
+            if (it.trim() == "noop") {
+                listOf(Operation())
+            } else {
+                listOf(Operation(), Operation(it.trim().split(" ").last().toInt()))
+            }
+        }
+        return operations
     }
-}
+
+    fun part2(input: String): String {
+        val operations = extractOperations(input)
+        var crtCenter = 1
+        val grid = List(6) { MutableList(40) { '.' } }
+        operations.forEachIndexed { index, operation ->
+            val currentCol = index % 40
+            val currentRow = index / 40
+            if((crtCenter - currentCol).absoluteValue <= 1){
+                grid[currentRow][currentCol] = '#'
+            }
+            crtCenter += operation.toAdd
+        }
+
+        return grid.print()
+        }
+    }
+
+    private fun List<MutableList<Char>>.print(): String {
+        return joinToString(separator = "\n") {
+            it.joinToString(separator = "")
+        }
+    }
 
 data class Operation(val toAdd: Int = 0) {}
