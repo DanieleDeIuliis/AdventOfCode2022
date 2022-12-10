befc7413960782f427eb141559bc21cf0e91279d
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2022-12-10 18:16:28 +0100

    day10 part 1

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -2,9 +2,35 @@ package day10
 
 object Day10 {
     fun part1(input: String): Int {
-        return 1234
+        var numberOfCycles = 0
+        var valueOfRegister = 1
+        var currentCycleToMultiply = 20
+        var result = 0
+        val operations = input.split("\n").flatMap {
+            if(it.trim() == "noop") {
+                listOf(Operation())
+            } else {
+                listOf(Operation(), Operation(it.trim().split(" ").last().toInt()))
+            }
+        }
+
+        operations.forEach {
+            numberOfCycles += 1
+            if (numberOfCycles == currentCycleToMultiply) {
+                result += currentCycleToMultiply * valueOfRegister
+                currentCycleToMultiply += 40
+            }
+            valueOfRegister += it.toAdd
+        }
+        if((numberOfCycles - 20) % 40 == 0) {
+            result += currentCycleToMultiply * valueOfRegister
+        }
+        return result
     }
+
     fun part2(input: String): Int {
         return 124
     }
 }
+
+data class Operation(val toAdd: Int = 0) {}
